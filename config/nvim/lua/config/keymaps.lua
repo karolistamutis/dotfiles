53615879f4e1fa24825909c9cfb652518012c701
Karolis Tamutis <karolis.t@gmail.com>
2025-04-15 18:41:14 +0300

    Use tab to navigate buffers in nvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<TAB>", function()
+  vim.cmd("bn!")
+end, { noremap = true, silent = true, desc = "Buffer next (force)" })
+vim.keymap.set("n", "<S-TAB>", function()
+  vim.cmd("bp!")
+end, { noremap = true, silent = true, desc = "Buffer previous (force)" })
